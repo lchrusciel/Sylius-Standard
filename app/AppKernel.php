1b41142cb73e13e878021bf2147c89785aa4a573
Łukasz Chruściel <lchrusciel@gmail.com>
2017-10-14 11:46:23 +0200

    Integrate ShopApi

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,9 @@ class AppKernel extends Kernel
 
             new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusAdminApiBundle.
             new \Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle(),
+
+            new \Sylius\ShopApiPlugin\ShopApiPlugin(),
+            new \League\Tactician\Bundle\TacticianBundle(),
         ];
 
         return array_merge(parent::registerBundles(), $bundles);
